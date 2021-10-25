23848ea62ded68db948c8049d82da9771a2a40f7
MetatransApps <kitopchiyski@gmail.com>
2021-10-25 15:11:51 +0300

    Next release last changes

--- app/src/main/java/com/maze_squirrel/main/Activity_Main.java
+++ app/src/main/java/com/maze_squirrel/main/Activity_Main.java
@@ -1,10 +1,10 @@
 package com.maze_squirrel.main;
 
 
+import org.metatrans.commons.IActivityInterstitial;
 import org.metatrans.commons.ads.api.IAdsConfiguration;
 import org.metatrans.commons.ads.impl.flow.IAdLoadFlow;
 import org.metatrans.commons.app.Application_Base_Ads;
-import org.metatrans.commons.app.IActivityInterstitial;
 import org.metatrans.commons.graphics2d.main.Activity_Main_Base2D;
 
 import android.view.View;
@@ -13,59 +13,13 @@ import android.view.View;
 public class Activity_Main extends Activity_Main_Base2D implements IActivityInterstitial {
 	
 	
-	private IAdLoadFlow current_adLoadFlow_Interstitial;
-	
-	
 	@Override
 	protected View createMainView() {
 		return new View_Main_4Controls_4Fire_Labyrints(this);
 	}
+
 	
-	
-	@Override
-	public void openInterstitial() {
-		try {
-			
-			System.out.println("Activity_Main openInterstitial called");
-			
-			if (current_adLoadFlow_Interstitial != null) {
-				System.out.println("Activity_Main openInterstitial RESUMED");
-				current_adLoadFlow_Interstitial.resume();
-			}
-			
-		} catch(Throwable t) {
-			t.printStackTrace();
-		}
-	}
-	
-	
-	@Override
-	protected void onResume() {
-		
-		super.onResume();
-		
-		if (getInterstitialName() != null) {
-			
-			current_adLoadFlow_Interstitial = ((Application_Base_Ads)getApplication()).getAdsManager().getCachedFlow(getInterstitialName());
-			
-			if (current_adLoadFlow_Interstitial == null) {
-				
-				System.out.println("Activity_Main create Interstitial");
-				
-				current_adLoadFlow_Interstitial = ((Application_Base_Ads)getApplication()).getAdsManager().createFlow_Interstitial_Mixed(getInterstitialName());
-				((Application_Base_Ads)getApplication()).getAdsManager().putCachedFlow(getInterstitialName(), current_adLoadFlow_Interstitial);
-			} else {
-				
-				System.out.println("Activity_Main Interstitial EXISTS");
-				
-				//current_adLoadFlow_Interstitial.cleanCurrent();
-				current_adLoadFlow_Interstitial.pause();
-			}
-		}
-	}
-	
-	
-	private String getInterstitialName() {
+	public String getInterstitialName() {
 		return IAdsConfiguration.AD_ID_INTERSTITIAL1;
 	}
 }
