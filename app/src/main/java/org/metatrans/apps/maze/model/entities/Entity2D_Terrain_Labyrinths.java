e22c7cb6199eff1a61d86dd3ece4159b6d5a2dcb
MetatransApps <kitopchiyski@gmail.com>
2022-05-21 20:16:10 +0300

    Clear code

--- app/src/main/java/org/metatrans/apps/maze/model/entities/Entity2D_Terrain_Labyrinths.java
+++ app/src/main/java/org/metatrans/apps/maze/model/entities/Entity2D_Terrain_Labyrinths.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.RectF;
 
 import org.metatrans.apps.maze.app.Application_Maze;
-import org.metatrans.apps.maze.model.World_Labyrints;
 import org.metatrans.commons.app.Application_Base;
 import org.metatrans.commons.graphics2d.model.IWorld;
 import org.metatrans.commons.graphics2d.model.entities.Entity2D_Ground;
@@ -29,24 +28,14 @@ public abstract class Entity2D_Terrain_Labyrinths extends Entity2D_Ground {
 
 		boolean is_border = world.isOuterBorder(getCellIndex_X(), getCellIndex_Y());
 
-		return flag1 ? ((World_Labyrints) Application_Maze.getInstance().getWorld()).getBitmap_wall(!is_border)
-				: ((World_Labyrints) Application_Maze.getInstance().getWorld()).getBitmap_wall(is_border);
+		return flag1 ? (Application_Maze.getInstance().getWorld()).getBitmap_wall(!is_border)
+				: (Application_Maze.getInstance().getWorld()).getBitmap_wall(is_border);
 	}
 
 
 	protected boolean getFlag1() {
 
 		return true;
-
-		/*throw new UnsupportedOperationException();
-
-		int currOrderNumber = ConfigurationUtils_Dinosaurs.getOrderNumber(((UserSettings_Dinosaurs) Application_Base.getInstance().getUserSettings()).dinosaurs_id);
-		int dinosaurs_id = ConfigurationUtils_Dinosaurs.getID(currOrderNumber);
-		IConfigurationDinosaurs dinosaurs_cfg = (IConfigurationDinosaurs) ConfigurationUtils_Dinosaurs.getConfigByID(dinosaurs_id);
-		boolean flag1 = dinosaurs_cfg.likesFlowers();
-
-		return flag1;
-		*/
 	}
 
 
@@ -67,7 +56,6 @@ public abstract class Entity2D_Terrain_Labyrinths extends Entity2D_Ground {
 
 		//System.out.println("getCellIndex_X()=" + getCellIndex_X() + ", getCellIndex_Y()=" + getCellIndex_Y() + ", terrain_entity=" + terrain_entity);
 
-
 		if (terrain_entity instanceof Entity2D_Terrain_Empty_Labyrinths) {
 
 			throw new UnsupportedOperationException("terrain_entity instanceof Entity2D_Terrain_Empty_Labyrinths");
@@ -81,28 +69,4 @@ public abstract class Entity2D_Terrain_Labyrinths extends Entity2D_Ground {
 			throw new IllegalStateException("terrain_entity = " + terrain_entity);
 		}
 	}
-
-
-	/*@Override
-	public Bitmap getBitmap() {
-
-		Entity2D_Ground terrain_entity = world.getTerrainCell(getCellIndex_X(), getCellIndex_Y());
-
-		if (terrain_entity instanceof Entity2D_Terrain_Empty_Labyrinths) {
-
-			throw new UnsupportedOperationException("terrain_entity instanceof Entity2D_Terrain_Empty_Labyrinths");
-
-		} else if (terrain_entity instanceof Entity2D_Terrain_Wall_Labyrinths) {
-
-			return ((World_Labyrints) Application_Maze.getInstance().getWorld()).getBitmap_wall_1();
-
-		} else if (terrain_entity instanceof Entity2D_Ground_Wall_Border_Labyrinths) {
-
-			return ((World_Labyrints) Application_Maze.getInstance().getWorld()).getBitmap_wall_2();
-
-		} else {
-
-			throw new IllegalStateException("terrain_entity = " + terrain_entity);
-		}
-	}*/
 }
