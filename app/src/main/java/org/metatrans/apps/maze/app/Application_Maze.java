a27b3ceb60376cb5430503f96d567b80a1d78594
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2022-12-17 21:27:54 +0200

    Remove unused import

--- app/src/main/java/org/metatrans/apps/maze/app/Application_Maze.java
+++ app/src/main/java/org/metatrans/apps/maze/app/Application_Maze.java
@@ -20,7 +20,6 @@ import org.metatrans.commons.events.api.IEventsManager;
 import org.metatrans.commons.graphics2d.app.Application_2D_Base;
 import org.metatrans.commons.graphics2d.model.GameData;
 import org.metatrans.commons.graphics2d.model.IWorld;
-import org.metatrans.commons.model.BitmapCache_Base;
 import org.metatrans.commons.model.GameData_Base;
 import org.metatrans.commons.model.UserSettings_Base;
 import org.metatrans.commons.storage.StorageUtils;
